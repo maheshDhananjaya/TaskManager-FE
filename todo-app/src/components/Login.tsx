b3b0657bdbaab3b2db88bb1e052ba1b593a7f7de
maheshKulathunga <mahesh.k@aeturnum.com>
2025-10-25 12:28:08 +0530

    add login page UI

--- todo-app/src/components/Login.tsx
+++ todo-app/src/components/Login.tsx
@@ -1,9 +1,23 @@
-import React, { FC } from "react";
+import React, {FC, FormEvent, useState} from "react";
+import {useRouter} from "next/router";
 
 const LoginPage:FC = ()=>{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+    const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        console.log("form submitted")
+        router.push("/search")
+    }
     return (
-        <div>
-            login page
+        <div className={"flex justify-center flex-col border rounded-sm py-10 px-40"}>
+            <div className={"my-4 text-2xl font-bold flex justify-center"}>TM</div>
+           <form onSubmit={handleSubmit} className={"flex flex-col items-center gap-4 px-4"}>
+               <input type="text" className="border border-gray-200 p-2 w-full" placeholder="User Name"  />
+               <input type="text" className="border border-gray-200 p-2 w-full" placeholder="Password" />
+               <button className="border rounded-md px-4 py-2 bg-amber-500 cursor-pointer" type="submit">Submit</button>
+           </form>
         </div>
     )
 }
--- todo-app/src/pages/index.tsx
+++ todo-app/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import Todo from "@/components/Todo";
+import Login from "@/components/Login";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,8 +15,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <div className="flex p-7 content-center justify-center">
-      <Todo />
+    <div className="flex justify-center flex-col p-40">
+     <Login/>
     </div>
   );
 }
