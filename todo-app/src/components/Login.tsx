695cb7343f5ce4414174ead46fbfeb085ae8e227
maheshKulathunga <mahesh.k@aeturnum.com>
2025-10-25 23:38:26 +0530

    feature : add firebase authentication

--- todo-app/src/components/Login.tsx
+++ todo-app/src/components/Login.tsx
@@ -1,22 +1,37 @@
 import React, {FC, FormEvent, useState} from "react";
 import {useRouter} from "next/router";
+import {signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/lib/firebase";
 
-const LoginPage:FC = ()=>{
+const LoginPage: FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    // const [error, setError] = useState("");
     const router = useRouter();
-    const handleSubmit = (e:FormEvent<HTMLFormElement>)=>{
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        router.push("/search")
-    }
+        try {
+            await signInWithEmailAndPassword(auth, email, password).then(() => {
+                router.push("/search")
+            });
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <div className={"flex justify-center flex-col border rounded-sm py-10 px-40"}>
             <div className={"my-4 text-2xl font-bold flex justify-center"}>TM</div>
-           <form onSubmit={handleSubmit} className={"flex flex-col items-center gap-4 px-4"}>
-               <input type="text" className="border border-gray-200 p-2 w-full" placeholder="User Name"  />
-               <input type="text" className="border border-gray-200 p-2 w-full" placeholder="Password" />
-               <button className="border rounded-md px-4 py-2 bg-amber-500 cursor-pointer" type="submit">Submit</button>
-           </form>
+            <form onSubmit={handleSubmit} className={"flex flex-col items-center gap-4 px-4"}>
+                <input type="text" className="border border-gray-200 p-2 w-full" placeholder="User Email" value={email}
+                       onChange={(e) => setEmail(e.target.value)}/>
+                <input type="password" className="border border-gray-200 p-2 w-full" placeholder="Password"
+                       value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <button className="border rounded-md px-4 py-2 bg-amber-500 cursor-pointer" type="submit">Submit
+                </button>
+            </form>
         </div>
     )
 }
--- todo-app/src/pages/index.tsx
+++ todo-app/src/pages/index.tsx
@@ -1,17 +1,6 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import Todo from "@/components/Todo";
+
 import Login from "@/components/Login";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
